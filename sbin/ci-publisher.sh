020ec1da895ad83b19571488eacfe10eee0d4156
Nikolas Thitu <nikolas@hauz.io>
2017-12-20 15:50:36 -0500

    ci housekeeping update

--- sbin/ci-publisher.sh
+++ sbin/ci-publisher.sh
@@ -3,7 +3,7 @@
 branch=$1
 event=$2
 
-cnt=`echo $branch | egrep -e "(^master$|^release$|^.*-snapshot$)" | wc -l`
+cnt=`echo $branch | egrep -e "(^master.*$|^release.*$)" | wc -l`
 
 if [ $cnt -eq 1 ] && [ "$event" = "push" ]; then
   ./gradlew artifactoryPublish clean
